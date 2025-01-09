499c4382144675edf2ce71e7747d6f01b8049ce3
viaguadona <aguadonavarrovictor@gmail.com>
2025-01-09 20:35:44 +0100

    revisar últimos detalles del ex2

--- src/n2exercici1/Entrada.java
+++ src/n2exercici1/Entrada.java
@@ -8,36 +8,49 @@ public class Entrada {
 	
 		
 	public static byte llegirByte(String missatge) throws  InputMismatchException {
+		System.out.println(missatge);
 		return scan.nextByte();
 		
 	}
 	
 	public static int llegirInt(String missatge) throws InputMismatchException {
+		
+		System.out.println(missatge);
 		return scan.nextInt();
 		
 	}
 	
 	public static float llegirFloat(String missatge) throws InputMismatchException {
+		
+		System.out.println(missatge);
 		return scan.nextFloat();
 		
 	}
 
 	public static double llegirDouble(String missatge) throws InputMismatchException {
+		
+		System.out.println(missatge);
 		return scan.nextDouble();
 		
 	}
 	
 	public static char llegirChar(String missatge) throws Exception {
+		
+		System.out.println(missatge);
 		return scan.next().charAt(0);
 		
 	}
 	
 	public static String llegirString(String missatge) throws Exception {
+		
+		System.out.println(missatge);
 		return scan.next();
 		
 	}
 	
 	public static boolean llegirSiNo(String missatge) throws Exception {
+		
+		System.out.println(missatge);
 		String answere = scan.next();
 		
 		if(answere.equalsIgnoreCase("s")) {
--- src/n2exercici1/Main.java
+++ src/n2exercici1/Main.java
@@ -1,10 +1,36 @@
 package n2exercici1;
 
+import java.util.InputMismatchException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean ok = false;
+		while(!ok) {	
+			try {
+				Entrada.llegirByte("Introdueix la teva edat en Bytes: ");
+				ok = true;
+				
+			}catch(InputMismatchException e) {
+				System.out.println("ERROR: " + e.getMessage());
+			}finally {
+				Entrada.scan.nextLine();
+			}
+		}
+		
+		ok=false;
+		while(!ok) {	
+			try {
+				Entrada.llegirInt("Introdueix la teva edat: ");
+				ok = true;
+				
+			}catch(InputMismatchException e) {
+				System.out.println("ERROR: " + e.getMessage());
+			}finally {
+				Entrada.scan.nextLine();
+			}
+		}
 	}
 
 }
