1aa12ae833c7e4c35ad3fbba7ae81cf6e189cadc
viaguadona <aguadonavarrovictor@gmail.com>
2025-01-14 17:38:56 +0100

    Exercice 2 completed

--- src/n2exercici1/Entrada.java
+++ src/n2exercici1/Entrada.java
@@ -7,57 +7,133 @@ public class Entrada {
 	static Scanner scan = new Scanner(System.in);
 	
 		
-	public static byte llegirByte(String missatge) throws  InputMismatchException {
-		System.out.println(missatge);
-		return scan.nextByte();
+	public static byte llegirByte(String missatge) {
 		
+		byte b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.nextByte();
+				return b;
+			}catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}	
 	}
 	
-	public static int llegirInt(String missatge) throws InputMismatchException {
+	public static int llegirInt(String missatge) {
 		
-		System.out.println(missatge);
-		return scan.nextInt();
-		
+		int b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.nextInt();
+				return b;
+			}catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}
 	}
 	
-	public static float llegirFloat(String missatge) throws InputMismatchException {
+	public static float llegirFloat(String missatge) {
 		
-		System.out.println(missatge);
-		return scan.nextFloat();
+		float b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.nextFloat();
+				return b;
+			}catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}
 		
 	}
 
-	public static double llegirDouble(String missatge) throws InputMismatchException {
+	public static double llegirDouble(String missatge) {
 		
-		System.out.println(missatge);
-		return scan.nextDouble();
-		
-	}
-	
-	public static char llegirChar(String missatge) throws Exception {
-		
-		System.out.println(missatge);
-		return scan.next().charAt(0);
-		
-	}
-	
-	public static String llegirString(String missatge) throws Exception {
-		
-		System.out.println(missatge);
-		return scan.next();
-		
-	}
-	
-	public static boolean llegirSiNo(String missatge) throws Exception {
-		
-		System.out.println(missatge);
-		String answere = scan.next();
-		
-		if(answere.equalsIgnoreCase("s")) {
-			return  true;
-		}else {
-			return false;
+		double b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.nextDouble();
+				return b;
+			}catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
 		}
 		
+	}
+	
+	public static char llegirChar(String missatge) {
+		
+		String b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.next();
+				if(b.length() > 1) {
+					throw new Exception("Ha de ser un sol caràcter.");
+				}
+				
+				return b.charAt(0);
+				
+			}catch(Exception e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}
+		
+	}
+	
+	public static String llegirString(String missatge) {
+		
+		String b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.next();
+				if(!(b instanceof String)) {
+					throw new Exception("Ha de ser una paraula.");
+				}
+				
+				return b;
+				
+			}catch(Exception e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}
+		
+	}
+	
+	public static boolean llegirSiNo(String missatge) {
+		
+		String b;
+		while(true) {
+			System.out.println(missatge);
+			try {
+				b = scan.next();
+				if(b.length() > 1) {
+					throw new Exception("Ha de ser un sol caràcter.");
+				}
+				
+				if(b.equalsIgnoreCase("s")) {
+					return  true;
+				}else if(b.equalsIgnoreCase("n")){
+					return false;
+				}else {
+					throw new Exception("Ha de se 's' o 'n' la resposta.");
+				}
+				
+			}catch(Exception e) {
+				scan.nextLine();
+				System.out.println("ERROR: " + e.getMessage());
+			}
+		}
 	}
 }
--- src/n2exercici1/Main.java
+++ src/n2exercici1/Main.java
@@ -1,36 +1,25 @@
 package n2exercici1;
 
-import java.util.InputMismatchException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean ok = false;
-		while(!ok) {	
-			try {
-				Entrada.llegirByte("Introdueix la teva edat en Bytes: ");
-				ok = true;
-				
-			}catch(InputMismatchException e) {
-				System.out.println("ERROR: " + e.getMessage());
-			}finally {
-				Entrada.scan.nextLine();
-			}
-		}
 		
-		ok=false;
-		while(!ok) {	
-			try {
-				Entrada.llegirInt("Introdueix la teva edat: ");
-				ok = true;
+		Entrada.llegirByte("Introdueix la teva edat en Bytes: ");
 				
-			}catch(InputMismatchException e) {
-				System.out.println("ERROR: " + e.getMessage());
-			}finally {
-				Entrada.scan.nextLine();
-			}
-		}
+		Entrada.llegirInt("Introdueix la teva edat: ");
+		
+		Entrada.llegirFloat("Introdueix un número decimal: ");
+		
+		Entrada.llegirDouble("Introdueix un altre: ");
+		
+		Entrada.llegirChar("Introdueix caràcter: ");
+		
+		Entrada.llegirString("Introdueix una paralua: ");
+		
+		Entrada.llegirSiNo("Ets home? ");
+		
 	}
 
 }
